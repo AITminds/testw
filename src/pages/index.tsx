2aedbedef4572d27404140a002c3dbc602319333
AITminds <dib7.alex@gmail.com>
2025-03-13 16:13:40 +0300

    test

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <div className="w-32 h-32 bg-black"></div>
+    </div>
+  );
+}
